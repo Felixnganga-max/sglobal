ac77613467104da60575f1037fa7a71d1a6ade0d
Felixnganga-max <felixngunga3620@gmail.com>
2026-02-16 12:12:49 +0300

    Added

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // ✅ CORS - MUST BE BEFORE OTHER MIDDLEWARE
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "https://smartglobal-w1ni.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,

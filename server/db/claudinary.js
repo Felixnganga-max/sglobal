c9a7bb03b4a97676cd7255b971a81ad72541dc66
Felix Ngunga <felixngunga22@gmail.com>
2026-03-07 19:14:56 +0300

    uri

--- server/db/claudinary.js
+++ server/db/claudinary.js
@@ -4,10 +4,9 @@ const multer = require("multer");
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dfxanfjjd",
-  api_key: process.env.CLOUDINARY_API_KEY || "237949519786658",
-  api_secret:
-    process.env.CLOUDINARY_API_SECRET || "QT1vD2PK_5Yd8QO4XyP3TfCjtdw",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // Configure Multer storage for Cloudinary
